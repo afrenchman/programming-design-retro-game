c526f7082c70ed322e72ddf4bb1a78a5ab4f1f24
Chaital Gummalla <112448060+afrenchman@users.noreply.github.com>
2025-04-30 14:00:50 -0400

    Update main.py

--- FinalContra/main.py
+++ FinalContra/main.py
@@ -2,13 +2,12 @@ import os
 import random
 import sys
 import pygame
-
+from settings import *
+from sprites import *
+from camera import *
+from graphics import *
 
 def run_fc():
-	from settings import *
-	from sprites import *
-	from camera import *
-	from graphics import *
 	class Game:
 		def __init__(self):
 			# Initialize
