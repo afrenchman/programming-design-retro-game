2dc521074e0131ffb8b8640a2c227d445cc4f06e
Chaital Gummalla <112448060+afrenchman@users.noreply.github.com>
2025-04-29 23:55:35 -0400

    Update camera.py

--- FinalContra/camera.py
+++ FinalContra/camera.py
@@ -5,16 +5,16 @@ vec = pygame.math.Vector2
 
 class Camera(object):
     def __init__(self, width, height):
-    	self.pos = vec(0,0)
+        self.pos = vec(0,0)
     def update(self,sprite):
-    	if sprite.canMove:
-    		self.pos.x = -sprite.pos.x
-    	if self.pos.x >= LEFT_BOUND:
-    		self.pos.x = LEFT_BOUND
-    	elif self.pos.x <= RIGHT_BOUND:
-    		self.pos.x = RIGHT_BOUND
-    	#self.pos.y = min(-sprite.pos.y+HEIGHT/2,0)
-    	#print(self.pos.x)
-   
+        if sprite.canMove:
+            self.pos.x = -sprite.pos.x
+        if self.pos.x >= LEFT_BOUND:
+            self.pos.x = LEFT_BOUND
+        elif self.pos.x <= RIGHT_BOUND:
+            self.pos.x = RIGHT_BOUND
+        #self.pos.y = min(-sprite.pos.y+HEIGHT/2,0)
+        #print(self.pos.x)
+
 camera = Camera(WIDTH,HEIGHT)
-        
+
