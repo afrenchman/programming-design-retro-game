89bdf6d16dc6abd4fb0de7d706f3d90f7c559b19
jennasannguyen <sann@usf.edu>
2025-04-30 13:06:33 -0400

    Add files via upload

--- Tetris/main.py
+++ Tetris/main.py
@@ -1,11 +1,342 @@
 import pygame,sys
-from game import Game
-from colors import Colors
-from button import Button
+import random
 import os
-import main
+import main 
 
 pygame.init()
+
+class Colors:
+    dark_grey = (26,31,40)
+    green = (47,230,23)
+    red = (232,18,18)
+    orange = (226,116,17)
+    yellow = (237,234,4)
+    purple = (166,0,247)
+    cyan = (21,204,209)
+    blue = (13,64,216)
+    white = (255,255,255)
+    dark_blue = (44,44,127)
+    light_blue = (59,85,162)
+    black = (0,0,0)
+
+    @classmethod
+    def get_cell_colors(cls):
+        return [cls.dark_grey, cls.green, cls.red, cls.orange, cls.yellow, cls.purple, cls.cyan, cls.blue]
+    
+class Position:
+    def __init__(self, row, column):
+        self.row = row 
+        self.column = column
+
+class Block:
+    def __init__(self, id):
+        self.id = id
+        self.cells = {}
+        self.cell_size = 35
+        self.row_offset = 0
+        self.column_offset = 0
+        self.rotation_state = 0
+        self.colors = Colors.get_cell_colors()
+
+    def move(self, rows, columns):
+        self.row_offset += rows
+        self.column_offset += columns
+
+    def get_cell_positions(self):
+        tiles = self.cells[self.rotation_state]
+        moved_tiles = []
+        for position in tiles:
+            position = Position(position.row + self.row_offset, position.column + self.column_offset)
+            moved_tiles.append(position)
+        return moved_tiles
+    
+    def rotate(self):
+        self.rotation_state += 1
+        if self.rotation_state == len(self.cells):
+            self.rotation_state = 0
+
+    def undo_rotation(self):
+        self.rotation_state -= 1
+        if self.rotation_state == 0:
+            self.rotation_state = len(self.cells) - 1
+
+    def draw(self, screen, offset_x, offset_y):
+        tiles = self.get_cell_positions()
+        for tile in tiles:
+            tile_rect = pygame.Rect(offset_x + tile.column*self.cell_size, offset_y + tile.row*self.cell_size, self.cell_size - 1, self.cell_size - 1)
+            pygame.draw.rect(screen, self.colors[self.id], tile_rect)
+
+class Lblock(Block):
+    def __init__(self):
+        super().__init__(id = 1)
+        self.cells = {
+            0: [Position(0,2), Position(1,0), Position(1,1), Position(1,2)],
+            1: [Position(0,1), Position(1,1), Position(2,1), Position(2,2)],
+            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,0)],
+            3: [Position(0,0), Position(0,1), Position(1,1), Position(2,1)],
+        }
+        self.move(0,3)
+
+class Jblock(Block):
+    def __init__(self):
+        super().__init__(id = 2)
+        self.cells = {
+            0: [Position(0,0), Position(1,0), Position(1,1), Position(1,2)],
+            1: [Position(0,1), Position(0,2), Position(1,1), Position(2,1)],
+            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,2)],
+            3: [Position(0,1), Position(1,1), Position(2,0), Position(2,1)],
+        }
+        self.move(0,3)
+
+class Iblock(Block):
+    def __init__(self):
+        super().__init__(id = 3)
+        self.cells = {
+            0: [Position(1,0), Position(1,1), Position(1,2), Position(1,3)],
+            1: [Position(0,2), Position(1,2), Position(2,2), Position(3,2)],
+            2: [Position(2,0), Position(2,1), Position(2,2), Position(2,3)],
+            3: [Position(0,1), Position(1,1), Position(2,1), Position(3,1)],
+        }
+        self.move(-1,3)
+
+class Oblock(Block):
+    def __init__(self):
+        super().__init__(id = 4)
+        self.cells = {
+            0: [Position(0,0), Position(0,1), Position(1,0), Position(1,1)],
+        }
+        self.move(0,4)
+
+class Sblock(Block):
+    def __init__(self):
+        super().__init__(id = 5)
+        self.cells = {
+            0: [Position(0,1), Position(0,2), Position(1,0), Position(1,1)],
+            1: [Position(0,1), Position(1,1), Position(1,2), Position(2,2)],
+            2: [Position(1,1), Position(1,2), Position(2,0), Position(2,1)],
+            3: [Position(0,0), Position(1,0), Position(1,1), Position(2,1)],
+        }
+        self.move(0,3)
+
+class Tblock(Block):
+    def __init__(self):
+        super().__init__(id = 6)
+        self.cells = {
+            0: [Position(0,1), Position(1,0), Position(1,1), Position(1,2)],
+            1: [Position(0,1), Position(1,1), Position(1,2), Position(2,1)],
+            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,1)],
+            3: [Position(0,1), Position(1,0), Position(1,1), Position(2,1)],
+        }
+        self.move(0,3)
+
+class Zblock(Block):
+    def __init__(self):
+        super().__init__(id = 7)
+        self.cells = {
+            0: [Position(0,0), Position(0,1), Position(1,1), Position(1,2)],
+            1: [Position(0,2), Position(1,1), Position(1,2), Position(2,1)],
+            2: [Position(1,0), Position(1,1), Position(2,1), Position(2,2)],
+            3: [Position(0,1), Position(1,0), Position(1,1), Position(2,0)],
+        }
+        self.move(0,3)
+
+class Button():
+	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+		self.image = image
+		self.x_pos = pos[0]
+		self.y_pos = pos[1]
+		self.font = font
+		self.base_color = base_color
+		self.hovering_color = hovering_color
+		self.text_input = text_input
+		self.text = self.font.render(self.text_input, True, self.base_color)
+		
+		if self.image is None:
+			self.image = self.text
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+	def update(self, screen):
+		if self.image is not None:
+			screen.blit(self.image, self.rect)
+		screen.blit(self.text, self.text_rect)
+
+	def checkForInput(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			return True
+		return False
+
+	def changeColor(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.text = self.font.render(self.text_input, True, self.hovering_color)
+		else:
+			self.text = self.font.render(self.text_input, True, self.base_color)
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+class Game:
+    def __init__(self):
+        self.grid = Grid()
+        self.blocks = [Iblock(), Jblock(), Lblock(), Oblock(), Sblock(), Tblock(), Zblock()]
+        self.current_block = self.get_random_block()
+        self.next_block = self.get_random_block()
+        self.game_over = False
+        self.score = 0
+        self.rotate_sound = pygame.mixer.Sound(current_dir + "/sounds/rotate.ogg")
+        self.clear_sound = pygame.mixer.Sound(current_dir + "/sounds/clear.ogg")
+
+        pygame.mixer.music.load(current_dir + "/sounds/tetris_soundtrack.mp3")
+        pygame.mixer.music.play(-1)
+
+    def update_score(self, rows_cleared, move_down_points):
+        if rows_cleared == 1:
+            self.score += 50
+        elif rows_cleared == 2:
+            self.score += 120
+        elif rows_cleared == 3:
+            self.score += 300
+        elif rows_cleared == 4:
+            self.score += 500
+        self.score += move_down_points
+
+    def get_random_block(self):
+        if len(self.blocks) == 0:
+            self.blocks = [Iblock(), Jblock(), Lblock(), Oblock(), Sblock(), Tblock(), Zblock()]
+        block = random.choice(self.blocks)
+        self.blocks.remove(block)
+        return block
+    
+    def move_left(self):
+        self.current_block.move(0,-1)
+        if self.block_inside() == False or self.block_fits() == False:
+            self.current_block.move(0,1)
+
+    def move_right(self):
+        self.current_block.move(0,1)
+        if self.block_inside() == False or self.block_fits() == False:
+            self.current_block.move(0,-1)
+
+    def move_down(self):
+        self.current_block.move(1,0)
+        if self.block_inside() == False or self.block_fits() == False:
+            self.current_block.move(-1,0)
+            self.lock_block()
+
+    def lock_block(self):
+        tiles = self.current_block.get_cell_positions()
+        for position in tiles:
+            self.grid.grid[position.row][position.column] = self.current_block.id
+        self.current_block = self.next_block
+        self.next_block = self.get_random_block()
+        rows_cleared = self.grid.clear_full_rows()
+        if rows_cleared > 0:
+            self.clear_sound.play()
+            self.update_score(rows_cleared, 0)
+        if self.block_fits() == False:
+            self.game_over = True
+
+    def reset(self):
+        self.grid.reset()
+        self.blocks = [Iblock(), Jblock(), Lblock(), Oblock(), Sblock(), Tblock(), Zblock()]
+        self.current_block = self.get_random_block()
+        self.next_block = self.get_random_block()
+        self.score = 0
+
+    def block_fits(self):
+        tiles = self.current_block.get_cell_positions()
+        for tile in tiles:
+            if self.grid.is_empty(tile.row, tile.column) == False:
+                return False
+        return True
+
+    def rotate(self):
+        self.current_block.rotate()
+        if self.block_inside() == False or self.block_fits() == False:
+            self.current_block.undo_rotation()
+        else:
+            self.rotate_sound.play()
+
+    def block_inside(self):
+        tiles = self.current_block.get_cell_positions()
+        for tile in tiles:
+            if self.grid.is_inside(tile.row, tile.column) == False:
+                return False
+        return True
+
+    def draw(self, screen):
+        self.grid.draw(screen)
+        self.current_block.draw(screen, 600, 170)
+
+        if self.next_block.id == 3:
+            self.next_block.draw(screen, 1111, 465)
+        elif self.next_block.id == 4:
+            self.next_block.draw(screen, 1111, 445)
+        else:   
+            self.next_block.draw(screen, 1129, 445)
+
+import pygame
+from colors import Colors
+
+class Grid:
+    def __init__(self):
+        self.num_rows = 20
+        self.num_cols = 10
+        self.cell_size = 35
+        self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
+        self.colors = Colors.get_cell_colors()
+
+    def print_grid(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                print(self.grid[row][column], end = " ")
+            print()
+
+    def is_inside(self, row, column):
+        if row >= 0 and row < self.num_rows and column >= 0 and column < self.num_cols:
+            return True
+        return False
+    
+    def is_empty(self, row, column):
+        if self.grid[row][column] == 0:
+            return True
+        return False
+    
+    def is_full(self, row):
+        for column in range(self.num_cols):
+            if self.grid[row][column] == 0:
+                return False
+        return True
+    
+    def clear_row(self, row):
+        for column in range(self.num_cols):
+            self.grid[row][column] == 0
+
+    def move_row_down(self, row, num_rows):
+        for column in range(self.num_cols):
+            self.grid[row+num_rows][column] = self.grid[row][column]
+            self.grid[row][column] = 0
+
+    def clear_full_rows(self):
+        completed = 0
+        for row in range(self.num_rows-1, 0, -1):
+            if self.is_full(row):
+                self.clear_row(row)
+                completed += 1
+            elif completed > 0:
+                self.move_row_down(row, completed)
+        return completed
+    
+    def reset(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                self.grid[row][column] = 0
+
+    def draw(self, screen):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                cell_value = self.grid[row][column]
+                cell_rect = pygame.Rect(column*self.cell_size + 600, row*self.cell_size + 170, self.cell_size - 1, self.cell_size - 1)
+                pygame.draw.rect(screen, self.colors[cell_value], cell_rect)
 
 def tetris_play():
     pygame.joystick.init()
